2dab0cb734cee90a9e2c9518b8b9d762c0b492bd
--global <--global>
2025-04-28 19:08:08 +0300

    Adding OPTIONS to Access-Control-Allow-Methods

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", origin);
   }
   res.header("Access-Control-Allow-Headers", "*");
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
   res.header("Access-Control-Allow-Credentials", true);
   next();
 });
